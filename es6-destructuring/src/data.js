89ae3916d8e399d7a7f67fac8b99b29faaa7c484
SchmidtRichard <ri.schmidta@gmail.com>
2021-05-27 18:54:08 +0100

    JavaScript ES6 Object and Array Destructuring - Continued

--- es6-destructuring/src/data.js
+++ es6-destructuring/src/data.js
@@ -1,3 +1,17 @@
-const animals = [ { name: "cat", sound: "meow" }, { name: "dog", sound: "woof" } ];
+const animals = [
+	{
+		name: "cat",
+		sound: "meow",
+		feedingRequirements: {
+			food: 2,
+			water: 3
+		}
+	},
+	{ name: "dog", sound: "woof" }
+];
+
+//setState
+//Accept an animal as an input in the function
+function useAnimails() {}
 
 export default animals;
--- es6-destructuring/src/index.js
+++ es6-destructuring/src/index.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import animals from "./data.js";
-import ReactDOM from "react-dom";
 
 /*
 Destructured Array
+
 Code below reaches inside the animals array and pulls out
 the first item and assigns a name of cat, pulls out the second
 item and assigns a name of dog to it
 */
 const [ cat, dog ] = animals;
-
+console.log("Destructuring an Array");
 console.log("Animals Array: " + animals);
 console.log("Cat: " + cat);
 console.log("Dog: " + dog);
@@ -19,9 +19,17 @@ Destructured Object
 */
 //const { name, sound } = cat;
 //can do the below instead of the above if you want to change the variables names
-const { name: catName, sound: catSound } = cat;
-console.log("Animal Name: " + catName);
-console.log(catName + " sound: " + catSound);
+// const { name: catName, sound: catSound } = cat;
+// console.log("Animal Name: " + catName);
+// console.log(catName + " sound: " + catSound);
+
+//Destructuring Nested Object
+//const { name, sound, feedingRequirements } = cat;
+// console.log(feedingRequirements);
+// console.log("Number of times we need to feed the cat: " + feedingRequirements.food);
+const { name, sound, feedingRequirements: { food, water } } = cat;
+console.log("\nDestructuring an Object");
+console.log("Number of times we need to feed the cat: " + food);
 
 // CHALLENGE: uncomment the code below and see the car stats rendered
 // import React from "react";
