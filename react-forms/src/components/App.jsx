b35178419786f97068208a9b1ae71a2bb72afff7
SchmidtRichard <ri.schmidta@gmail.com>
2021-06-09 13:06:38 +0100

    onChange event added, useState and destructure its output to hold name and the function setName

--- react-forms/src/components/App.jsx
+++ react-forms/src/components/App.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
-    </div>
-  );
+	//State - destructure the output for useState to hold two things: name and the function -> setName
+	const [ name, setName ] = useState("");
+
+	//Function that will be triggered every time the input is changed
+	function handleChange(event) {
+		console.log("Changed");
+		console.log("Value: " + event.target.value);
+		console.log("Placeholder: " + event.target.placeholder);
+		console.log("Type: " + event.target.type);
+
+		setName(event.target.value);
+	}
+
+	return (
+		<div className="container">
+			<h1>Hello {name}</h1>
+			<input type="text" onChange={handleChange} placeholder="What's your name?" value={name} />
+			<button>Submit</button>
+		</div>
+	);
 }
 
 export default App;
