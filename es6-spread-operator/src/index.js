354276b05d0e66140f6423132305ebd9e8032355
SchmidtRichard <ri.schmidta@gmail.com>
2021-06-30 12:18:41 +0100

    ES6 Spread Operator - e.g. add items of an array to another array

--- es6-spread-operator/src/index.js
+++ es6-spread-operator/src/index.js
@@ -3,3 +3,33 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const citrus = ["lime", "lemon", "orange"];
+
+//Use the ES6 spread operator to add the citrus fruits into the fruits array
+// const fruits = ["apple", "banana", "coconut", ...citrus];
+//Can also add in a different order, like in the example below
+const fruits = ["apple", ...citrus, "banana", "coconut"];
+
+console.log("Citrus Array: " + citrus);
+console.log("Frits Array: " + fruits);
+
+//We can also use the spread operator with objects as well
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
